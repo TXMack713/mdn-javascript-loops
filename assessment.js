4c886b18bb1452ef05ebee0d902df18b596499a7
TXMack713 <mackattack713@gmail.com>
2021-06-30 16:31:17 -0500

    3 files modified

---
+++ assessment.js
@@ -0,0 +1,57 @@
+// MDN JavaScript Loops Assessment 1 Code
+let myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+let list = document.createElement('ul');
+
+// Add your code here
+for (let i = 0, j = myArray.length; i < j; i += 1) {
+  let item = document.createElement('li');
+  item.textContent = myArray[i];
+  list.appendChild(item);
+}
+
+// Don't edit the code below here!
+
+const section = document.querySelector('.assessment1');
+section.appendChild(list);
+
+// MDN JavaScript Loops Assessment 2 Code
+let name = 'Mustafa';
+let i = 0;
+let para = document.createElement('p');
+
+let phonebook = [
+  { name: 'Chris', number: '1549' },
+  { name: 'Li Kang', number: '9634' },
+  { name: 'Anne', number: '9065' },
+  { name: 'Francesca', number: '3001' },
+  { name: 'Mustafa', number: '6888' },
+  { name: 'Tina', number: '4312' },
+  { name: 'Bert', number: '7780' },
+  { name: 'Jada', number: '2282' }
+];
+
+// Add your code here
+
+// Don't edit the code below here!
+let section2 = document.querySelector('.assessment2');
+section2.appendChild(para);
+
+// MDN JavaScript Loops Assessment 3 Code
+let k = 500;
+let para2 = document.createElement('p');
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Add your code here
+
+// Don't edit the code below here!
+let section3 = document.querySelector('.assessment3');
+section3.appendChild(para2);
